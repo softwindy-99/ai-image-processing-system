b7c5c8e026a70f36dfbdc4637c87e0d1d1e57466
Softwindy <softwindy@outlook.com>
2022-07-20 23:38:22 +0800

    bug修复

---
+++ server/interface/history.php
@@ -0,0 +1,91 @@
+<?php
+/*
+ *@Description: 服务使用记录 查询(get)
+ */
+
+/* 配置 */
+
+declare(strict_types=1); // 打开强类型模式
+$config_database = json_decode(file_get_contents("../config/database.json"));
+$config_upload = json_decode(file_get_contents("../config/upload.json"), true);
+
+/* 引入 */
+include "../lib/easy_mysqli.php";
+include "../lib/easy_response.php";
+include "../lib/tool.php";
+
+/* 请求类型和token */
+$method = $_SERVER['REQUEST_METHOD']; // string
+$headers = getallheaders();
+$token = $headers['Authorization']; // token
+if ($token != null)
+    $token = substr($token, 7); // 使用 PostMan 调试时有 "Bearer " 前缀，需要去除
+
+/* 数据库 */
+$con = new easy_mysqli(
+    $config_database->server_host,
+    $config_database->username,
+    $config_database->password,
+    $config_database->database_name,
+    $config_database->server_port,
+    $config_database->server_socket,
+);
+
+/* 响应 */
+$rep = new easy_response();
+$rep->set_content_type("application/json", "charset=UTF-8");
+
+/* 业务逻辑 */
+// 查询
+//  1. 检验 token
+//  2. 检验权限
+//  3. 查询
+$result = null;
+switch ($method) {
+    case "GET":
+        if ($token != null)
+            $user_id = check_token($token, $con);
+        else
+            $user_id = false;
+        if ($user_id != false) {
+            $user_permission = (int)$con->get_value_int("user_permission", "user_id", $user_id, "permission");
+            if ($user_permission < 2) {
+                $rows = $con->get_all_rows("user_history", ["server_id", "system", "time", "ip", "out_id", "in_id"]);
+                $result = $rows;
+                $rep->set_code(100);
+                $rep->set_status(true);
+                $rep->set_message("请求成功");
+                $rep->set_result($result);
+            } else {
+                $rep->set_code(200);
+                $rep->set_status(false);
+                $rep->set_message("权限不足");
+            }
+        } else {
+            $rep->set_code(200);
+            $rep->set_status(false);
+            $rep->set_message("token无效");
+        }
+        break;
+    case "POST":
+        $rep->set_code(200);
+        $rep->set_status(false);
+        $rep->set_message("无此操作");
+        break;
+    case "PUT":
+        $rep->set_code(200);
+        $rep->set_status(false);
+        $rep->set_message("无此操作");
+        break;
+    case "DELETE":
+        $rep->set_code(200);
+        $rep->set_status(false);
+        $rep->set_message("无此操作");
+        break;
+    default:
+        $rep->set_code(200);
+        $rep->set_status(false);
+        $rep->set_message("非法请求");
+}
+$rep->response();
+$con->close_connect();
